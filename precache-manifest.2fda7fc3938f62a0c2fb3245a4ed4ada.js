08a1c3b00ebe1266e89b38514fd95019d15fdb03
Miłosz Kaczmarek <miloszkaczmarek@hotmail.com>
2018-11-28 02:14:20 +0100

    Updates

---
+++ precache-manifest.2fda7fc3938f62a0c2fb3245a4ed4ada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "101a2900c6de14c121216d2d0809f1b3",
+    "url": "/Football/static/media/background.101a2900.png"
+  },
+  {
+    "revision": "5fda931d46c10cbdbb22",
+    "url": "/Football/static/js/runtime~main.5fda931d.js"
+  },
+  {
+    "revision": "b35b4c252b74eb40b3db",
+    "url": "/Football/static/js/main.b35b4c25.chunk.js"
+  },
+  {
+    "revision": "8ea8e5268f9080c7fc5d",
+    "url": "/Football/static/js/1.8ea8e526.chunk.js"
+  },
+  {
+    "revision": "b35b4c252b74eb40b3db",
+    "url": "/Football/static/css/main.64e7d066.chunk.css"
+  },
+  {
+    "revision": "042b8886a3a309d2e16600b7f92d3d85",
+    "url": "/Football/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/football/precache-manifest.a0d45b9c9cc329525695395ac5323b8c.js"
+  "/Football/precache-manifest.2fda7fc3938f62a0c2fb3245a4ed4ada.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/football/index.html", {
+workbox.routing.registerNavigationRoute("/Football/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
